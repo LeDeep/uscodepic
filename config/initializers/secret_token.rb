16dbcfbaec29a2b29813fa32165229ac4e2e80d1
Thomas Hopkins <hopkins.tc@gmail.com>
2013-04-14 22:38:35 -0700

    initial commit 'rails new uscodepic -d postgresql -T'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uscodepic::Application.config.secret_token = 'b2f63617d2637abb994a0841f633b6974d432704ef0fdfa1bf9f8a5b51c0f2777aaedd682620ec62a46e8fd70fdfa63faa1e1cc63b027e24a72af156e6c8fcde'
