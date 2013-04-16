151a2800870a7d8393ca935d0f4904cb7b109e74
Thomas Hopkins <hopkins.tc@gmail.com>
2013-04-15 19:59:37 -0700

    add user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'mass assignment' do
+    it {should allow_mass_assignemnt_of(:email)}
+    it {should allow_mass_assignemnt_of(:password)}
+    it {should allow_mass_assignemnt_of(:password_confirmation)}
+    it {should allow_mass_assignemnt_of(:remember_me)}
+    it {should allow_mass_assignemnt_of(:role)}
+  end
 end
